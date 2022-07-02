1a9cefdfd97e15d5c86df07a0c751e6b3c4b84b8
Alexander Pivovarov <pivovaa@amazon.com>
2022-07-02 04:35:40 +0000

    Add cmake build

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # dlc_tpm
+
+### Build
+To build dlc_tpm
+```
+mkdir build
+cd build
+cmake ..
+make -j$(nproc)
+```
+
+### Usage
+```
+./dlc_tpm -help
+```
--- src/dlc_tpm.cc
+++ src/dlc_tpm.cc
@@ -10,17 +10,19 @@
 #include <time.h>
 #include <vector>
 
+
 using std::cout;
 using std::endl;
 using std::string;
 using std::vector;
 
+std::string json_file = "test.json";
 std::string endpoint_url = "http://localhost:8080/invocations";
 int th_num = 1;
-int warmup_sec = 2;
-int win_sec = 10;
+int warmup_time = 2;
+int test_time = 10;
 void test_ping(std::string body_str);
-void tpm_runner(int thread_id, std::string body, time_t ss, int *my_cnt);
+void tpm_runner(int thread_id, const std::string& body, time_t ss, int *my_cnt);
 
 void readArgs(int argc, char **argv) {
   vector<string> args(argv + 1, argv + argc);
@@ -28,32 +30,42 @@ void readArgs(int argc, char **argv) {
   // Loop over command-line args
   for (auto i = args.begin(); i != args.end(); ++i) {
     if (*i == "-h" || *i == "--help") {
-      cout << "Syntax: dlc_tpm --url <url> --threads <n> --win_sec <n> "
-              "--warmup_sec <n>"
+      cout << "Syntax: dlc_tpm --json_file <file> --url <url> --threads <n> --test_time <n> "
+              "--warmup_time <n>"
            << endl;
       exit(1);
+    } else if (*i == "--json_file") {
+      json_file = *++i;
     } else if (*i == "--url") {
       endpoint_url = *++i;
     } else if (*i == "--threads") {
       th_num = stoi(*++i);
-    } else if (*i == "--warmup_sec") {
-      warmup_sec = stoi(*++i);
-    } else if (*i == "--win_sec") {
-      win_sec = stoi(*++i);
+    } else if (*i == "--warmup_time") {
+      warmup_time = stoi(*++i);
+    } else if (*i == "--test_time") {
+      test_time = stoi(*++i);
     }
   }
-  cout << "Endpoint: " << endpoint_url << "\n";
-  cout << "Warmup sec: " << warmup_sec << endl;
-  cout << "Measuring window sec: " << win_sec << endl;
+  cout << "Json file: " << json_file << endl;
+  cout << "Endpoint: " << endpoint_url << endl;
+  cout << "Number of threads: " << th_num << endl;
+  cout << "Warmup time sec: " << warmup_time << endl;
+  cout << "Test time sec: " << test_time << endl;
+  cout << "==================================================" << endl;
 }
 
 int main(int argc, char **argv) {
   readArgs(argc, argv);
-  std::ifstream t("test.json");
+  std::ifstream t(json_file);
+  if (!t.is_open()) {
+    std::cout << "\033[31m";
+    cout << "Error: Can not open file " << json_file << "\033[0m" << endl;
+    exit(3);
+  }
   std::stringstream buffer;
   buffer << t.rdbuf();
   t.close();
-  std::string body_str = buffer.str();
+  const std::string body_str = buffer.str();
 
   test_ping(body_str);
 
@@ -61,8 +73,7 @@ int main(int argc, char **argv) {
   int all_cnt[th_num];
   vector<std::thread> th_vector;
   for (int i = 0; i < th_num; i++) {
-    // std::thread t(tpm_runner, i, body_str);
-    th_vector.emplace_back(tpm_runner, i, body_str, ss, &(all_cnt[i]));
+    th_vector.emplace_back(tpm_runner, i, std::ref(body_str), ss, &(all_cnt[i]));
   }
   for (auto &t : th_vector) {
     t.join();
@@ -73,10 +84,14 @@ int main(int argc, char **argv) {
   for (int i = 0; i < th_num; i++) {
     total_cnt += all_cnt[i];
   }
-  float tpm = total_cnt * 60 / win_sec;
+  float tpm = total_cnt * 60 / test_time;
+  cout << "==================================================" << endl;
+  cout << "Endpoint: " << endpoint_url << endl;
+  cout << "Number of threads: " << th_num << endl;
+  cout << "Test time sec: " << test_time << endl;
   cout << "Total N of Transactions: " << total_cnt << endl;
-  cout << "Measuring window sec: " << win_sec << endl;
   printf("TPM: %.2f\n", tpm);
+  cout << "==================================================" << endl;
   return 0;
 }
 
@@ -87,21 +102,22 @@ void test_ping(std::string body_str) {
   cpr::Response resp = cpr::Post(url, header, body);
   std::cout << resp.status_code << endl;
   if (resp.status_code != 200) {
-    cout << "Bad response. Exit" << endl;
+    std::cout << "\033[31m";
+    cout << "Error: Bad response, code: " << resp.status_code << "\033[0m" << endl;
     exit(400);
   } else {
     cout << "Test ping: OK" << endl;
   }
 }
 
-void tpm_runner(int thread_id, std::string body_str, time_t ss, int *my_cnt) {
+void tpm_runner(int thread_id, const std::string& body_str, time_t ss, int *my_cnt) {
   cpr::Url url{endpoint_url};
   cpr::Body body{body_str};
   cpr::Header header{{"Content-Type", "application/json"}};
   int cnt = 0;
-  time_t start_time = ss + warmup_sec;
-  time_t stop_time = start_time + win_sec;
-  time_t break_time = stop_time + warmup_sec;
+  time_t start_time = ss + warmup_time;
+  time_t stop_time = start_time + test_time;
+  time_t break_time = stop_time + warmup_time;
   while (true) {
     cpr::Response resp = cpr::Post(url, header, body);
     time_t t0 = time(NULL);
